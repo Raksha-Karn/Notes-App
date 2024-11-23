d9e4c4221c34644a2ae7adc1b59948be06a6ab1b
raksh <karnraksha41@gmail.com>
2024-11-23 13:27:46 +0545

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/models/note_database.dart';
 import 'package:notes_app/pages/notes_page.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NoteDatabase.initialize();
   
-  runApp(const MainApp());
+  runApp(ChangeNotifierProvider(create: (context) => NoteDatabase(), child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:notes_app/models/note.dart';
 import 'package:path_provider/path_provider.dart';
 
-class NoteDatabase {
+class NoteDatabase extends ChangeNotifier {
   static late Isar isar;
 
   static Future<void> initialize() async {
@@ -28,6 +29,7 @@ class NoteDatabase {
     List<Note> fetchedNotes = await isar.notes.where().findAll();
     currentNotes.clear();
     currentNotes.addAll(fetchedNotes);
+    notifyListeners();
   }
 
   Future<void> updateNote(int id, String newText) async {
